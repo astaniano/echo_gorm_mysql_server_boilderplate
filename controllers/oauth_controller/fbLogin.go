012d2cf38a819f781d2ed26092f108ee2508f2a6
alex <debryniuk@gmail.com>
2021-04-22 01:39:09 +0300

    oauth was refactored (google and fb now have common methods => complied with dry principle)

--- controllers/oauth_controller/fbLogin.go
+++ controllers/oauth_controller/fbLogin.go
@@ -1,21 +1,17 @@
 package oauth_controller
 
 import (
-	"fmt"
 	"github.com/labstack/echo/v4"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/facebook"
-	"io/ioutil"
 	"myapp/constants"
 	"net/http"
-	"net/url"
 	"os"
 )
 
 var fbOauthConfig *oauth2.Config
 
 func FBLogin(c echo.Context) error {
-	// todo why &oauth2.Config ?????
 	fbOauthConfig = &oauth2.Config{
 		RedirectURL:  constants.HOST + constants.PORT + "/api/v1/login/fb/callback",
 		ClientID:     os.Getenv("FB_CLIENT_ID"),
@@ -29,34 +25,9 @@ func FBLogin(c echo.Context) error {
 }
 
 func FBLoginCallback(c echo.Context) error {
-	content, err := getUserInfoFB(c.FormValue("state"), c.FormValue("code"))
-	if err != nil {
-		fmt.Println(err.Error())
-		return c.Redirect(http.StatusTemporaryRedirect, "/")
-	}
-
-	return c.String(http.StatusOK, string(content))
-}
-
-func getUserInfoFB(state string, code string) ([]byte, error) {
-	if state != constants.OAUTH_STATE_STRING {
-		return nil, fmt.Errorf("invalid oauth state")
-	}
-	token, err := fbOauthConfig.Exchange(oauth2.NoContext, code)
-	if err != nil {
-		return nil, fmt.Errorf("code exchange failed: %s", err.Error())
-	}
-	// token.AccessToken: a0AfH6.......
-	response, err := http.Get("https://graph.facebook.com/me?access_token=" +
-		url.QueryEscape(token.AccessToken))
-	if err != nil {
-		return nil, fmt.Errorf("failed getting user info: %s", err.Error())
-	}
-	defer response.Body.Close()
-
-	contents, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		return nil, fmt.Errorf("failed reading response body: %s", err.Error())
-	}
-	return contents, nil
+	return oAuthCallbackHandler(
+		c,
+		"https://graph.facebook.com/me?access_token=",
+		fbOauthConfig,
+	)
 }
