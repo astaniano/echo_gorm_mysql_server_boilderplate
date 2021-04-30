845fb38b8101a30d44265b8526fc16cd7daaa98f
alex <debryniuk@gmail.com>
2021-04-30 16:32:02 +0300

    all tests are now independent of each other

--- models/user_test.go
+++ models/user_test.go
@@ -4,7 +4,6 @@ import (
 	"github.com/stretchr/testify/assert"
 	"myapp/database"
 	"myapp/helpers"
-	"os"
 	"testing"
 )
 
@@ -15,50 +14,44 @@ func TestHashPassword(t *testing.T) {
 
 	err := user.HashPassword(user.Password)
 	assert.NoError(t, err)
-
-	os.Setenv("passwordHash", user.Password)
 }
 
 func TestCreateUserRecord(t *testing.T) {
 	var userResult User
 
 	err := helpers.LoadEnvVariables()
-	if err != nil {
-		return
-	}
+	assert.NoError(t, err)
 	err = database.InitDatabase()
-	if err != nil {
-		t.Error(err)
-	}
-
-	err = database.DB.AutoMigrate(&User{})
 	assert.NoError(t, err)
 
 	user := User{
 		FirstName: "Test User FN",
 		LastName:  "Test User LN",
 		Email:     "test@email.com",
-		Password:  os.Getenv("passwordHash"),
+		Password:  "secret",
 	}
-
+	err = user.HashPassword(user.Password)
+	assert.NoError(t, err)
 	err = user.CreateUserRecord()
 	assert.NoError(t, err)
 
 	database.DB.Where("email = ?", user.Email).Find(&userResult)
-
-	database.DB.Unscoped().Delete(&user)
-
 	assert.Equal(t, "Test User FN", userResult.FirstName)
 	assert.Equal(t, "test@email.com", userResult.Email)
+
+	database.DB.Unscoped().Delete(&user)
 }
 
 func TestCheckPassword(t *testing.T) {
-	hash := os.Getenv("passwordHash")
-
 	user := User{
-		Password: hash,
+		FirstName: "Test User FN",
+		LastName:  "Test User LN",
+		Email:     "test@email.com",
+		Password:  "secret",
 	}
+	err := user.HashPassword(user.Password)
+	assert.NoError(t, err)
 
-	err := user.CheckPassword("secret")
+	err = user.CheckPassword("secret")
 	assert.NoError(t, err)
 }
